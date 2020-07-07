c2499e58c123d6d485f98d81014f111f70cf3420
nathPay <nathan.payre@gmail.com>
2020-07-07 16:22:24 +0200

    migration to confluence

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # iExecSlackBot
+
+Do this in iExecDailyBot channel slack:
+
+Create daily note:
+\daily rapport + script
+
+Add blockers:
+\blockers no
--- quickStart.py
+++ quickStart.py
@@ -1,9 +1,13 @@
-import gspread
+# import gspread
 import os
 import json
 import requests
 from slackeventsapi import SlackEventAdapter
 from slack import WebClient
+from atlassian import Confluence
+from tinydb import TinyDB, Query
+
+db = TinyDB('./db.json')
 
 SLACK_SIGNING_SECRET = os.environ["SLACK_SIGNING_SECRET"]
 slack_events_adapter = SlackEventAdapter(SLACK_SIGNING_SECRET, "/slack/events")
@@ -11,12 +15,22 @@ slack_events_adapter = SlackEventAdapter(SLACK_SIGNING_SECRET, "/slack/events")
 SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
 slack_client = WebClient(SLACK_BOT_TOKEN)
 
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-SPREADSHEET_ID = '1GELytQZ4vGt2v-yqhDkQrh3Lhu0PqvD_QcwiNey9Btc'
-gc = gspread.service_account()
-sh = gc.open("testSlackBot")
+CONFLUENCE_TOKEN = os.environ["CONFLUENCE_TOKEN"]
 
-CHANNEL = 'CPVF6A9RB'
+s = requests.Session()
+s.headers['Authorization'] = 'Bearer ' + CONFLUENCE_TOKEN
+
+confluence = Confluence(
+    url='https://iexecproject.atlassian.net/',
+    session=s)
+
+
+# SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+# SPREADSHEET_ID = '1GELytQZ4vGt2v-yqhDkQrh3Lhu0PqvD_QcwiNey9Btc'
+# gc = gspread.service_account()
+# sh = gc.open("testSlackBot")
+
+CHANNEL = '????'
 
 def getMembers():
     payload = {'token': SLACK_BOT_TOKEN, 'channel': CHANNEL}
@@ -46,8 +60,8 @@ def handle_message(event_data):
     # print(sh.sheet1.update('A2', user["user"]["real_name"]))
     # print(sh.sheet1.update('B2', message.get('text')))
     members = json.loads(getMembers())
-    for i in range(len(members["members"])):
-        print(sh.sheet1.update("A"+ str(i+2), json.loads(getUsername(members["members"][i]))["user"]["real_name"]))
+    # for i in range(len(members["members"])):
+    #     print(sh.sheet1.update("A"+ str(i+2), json.loads(getUsername(members["members"][i]))["user"]["real_name"]))
 
 slack_events_adapter.start(port=3030)
 
